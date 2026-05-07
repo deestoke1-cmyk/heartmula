5852bc0d05ff5e41267f7c5ca5e9b56e7f8571c2
dstoke <stokestoke89@gmail.com>
2026-05-07 17:38:00 -0500

    V21: Adjusted clap sequencing to half-time (4s intervals)

--- generate.py
+++ generate.py
@@ -1,39 +1,38 @@
 import os
 
-def generate_trap_drums(output_path):
-    print("🥁 Rendering V19: Layered Snare & Rhythmic Hats...")
+def generate_slow_clap_beat(output_path):
+    print("👏 Rendering V21: Slow Half-Time Claps...")
     
-    # 1. The 808 Bass (Lowered to 60Hz for more sub)
+    # 1. Triple-Layer Clap (Same texture, slower timing)
+    c1 = 'anoisesrc=d=0.08:c=white,highpass=f=1000,volume=8dB,adelay=0|0'
+    c2 = 'anoisesrc=d=0.08:c=white,highpass=f=1200,volume=6dB,adelay=10|10'
+    c3 = 'anoisesrc=d=0.1:c=white,highpass=f=800,volume=10dB,adelay=20|20'
+    
+    # 2. Foundation (808 & Hats)
     bass = 'sine=f=60:d=0.3,afade=t=out:st=0.1:d=0.2,volume=10dB'
-    
-    # 2. Layered Snare (The 'Crack' + The 'Body')
-    snare_body = 'sine=f=180:d=0.1,afade=t=out:st=0.05:d=0.05,volume=10dB'
-    snare_crack = 'anoisesrc=d=0.1:c=white,highpass=f=2000,volume=8dB'
-    
-    # 3. Metallic Hats (1/8th note rhythm with volume swing)
     hats = 'sine=f=8000:d=0.02,afade=t=out:st=0.01:d=0.01,tremolo=f=4:d=0.4,volume=5dB'
     
-    # 4. Sequencing Logic
+    # 3. Sequencing Logic (Changing loop size to 4.0 for half-time)
     filter_str = (
-        "[0]aloop=loop=60:size=0.5*44100[h]; "   # 1/8th hats
-        "[1]aloop=loop=15:size=2*44100,adelay=1000|1000[b]; " # 808 on the 1
-        "[2][3]amix=inputs=2[s_raw]; [s_raw]aloop=loop=15:size=2*44100,adelay=2000|2000[s]; " # Snare on the 2
-        "[h][b][s]amix=inputs=3"
+        "[0]aloop=loop=60:size=0.5*44100[h]; "
+        "[1]aloop=loop=15:size=2*44100,adelay=1000|1000[b]; "
+        "[2][3][4]amix=inputs=3[clap_raw]; [clap_raw]aloop=loop=7:size=4.0*44100,adelay=2000|2000[c]; "
+        "[h][b][c]amix=inputs=3"
     )
     
     cmd = (
         f"ffmpeg -hide_banner -loglevel error "
         f"-f lavfi -i '{hats}' -f lavfi -i '{bass}' "
-        f"-f lavfi -i '{snare_body}' -f lavfi -i '{snare_crack}' "
+        f"-f lavfi -i '{c1}' -f lavfi -i '{c2}' -f lavfi -i '{c3}' "
         f"-filter_complex \"{filter_str}\" "
         f"'{output_path}' -y"
     )
     os.system(cmd)
 
 if __name__ == '__main__':
-    output_path = "/Volumes/12121231/heartmula/outputs/HeartMuLa_V19.mp3"
+    output_path = "/Volumes/12121231/heartmula/outputs/HeartMuLa_V21.mp3"
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    generate_trap_drums(output_path)
+    generate_slow_clap_beat(output_path)
     if os.path.exists(output_path):
-        print("--- Playing: HeartMuLa_V19 (Drums) ---")
+        print("--- Playing: HeartMuLa_V21 (Slow Claps) ---")
         os.system(f"afplay '{output_path}'")
